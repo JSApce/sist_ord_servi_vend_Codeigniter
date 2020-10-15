99535e340e9e8fda1cba531e2290e7b51414b196
jsapce <jamerson.developer@gmail.com>
2020-10-14 22:00:05 -0300

    Fatiando o nosso template

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -7,9 +7,11 @@ class Home extends CI_Controller {
     public function __construct() {
         parent::__construct();
     }
-    
-    public function index() {
-        $this->load->view('home/index');
-    }
-}
 
+    public function index() {
+        $this->load->view('layout/header');
+        $this->load->view('home/index');
+        $this->load->view('layout/footer');
+    }
+
+}
