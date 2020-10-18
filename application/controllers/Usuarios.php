464fc20c5dd43c95da76851ee219e51c64472ef0
jsapce <jamerson.developer@gmail.com>
2020-10-18 16:50:57 -0300

    Criando o módulo sistema - Parte 3

--- application/controllers/Usuarios.php
+++ application/controllers/Usuarios.php
@@ -6,7 +6,11 @@ class Usuarios extends CI_Controller {
 
     public function __construct() {
         parent::__construct();
-        //TODO definir se existe sessão
+        
+        if (!$this->ion_auth->logged_in()) {
+            $this->session->set_flashdata('info', 'Sua sessão expirou!');
+            redirect('login');
+        }
     }
 
     public function index() {
