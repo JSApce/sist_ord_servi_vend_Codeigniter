a7c3c9e5676fff36aadb62ce880e85162ba01064
jsapce <jamerson.developer@gmail.com>
2020-11-02 11:40:29 -0300

    Cadastrando vendedor

--- application/controllers/Vendedores.php
+++ application/controllers/Vendedores.php
@@ -24,10 +24,6 @@ class Vendedores extends CI_Controller {
             'vendedores' => $this->core_model->get_all('vendedores'),
         );
 
-//        echo '<pre>';
-//        print_r($data['vendedores']);
-//        exit();
-
         $this->load->view('layout/header', $data);
         $this->load->view('vendedores/index');
         $this->load->view('layout/footer');
@@ -38,28 +34,6 @@ class Vendedores extends CI_Controller {
             $this->session->set_flashdata('error', 'Vendedor não encontrado');
             redirect('vendedores');
         } else {
-
-            /*
-              [vendedor_id] => 1
-              [vendedor_codigo] => 09842571
-              [vendedor_data_cadastro] => 2020-01-27 22:24:17
-              [vendedor_nome_completo] => Lucio Antonio de Souza
-              [vendedor_cpf] => 946.873.070-00
-              [vendedor_rg] => 36.803.319-3
-              [vendedor_telefone] =>
-              [vendedor_celular] => (41) 99999-9999
-              [vendedor_email] => vendedor@gmail.com
-              [vendedor_cep] => 80530-000
-              [vendedor_endereco] => Rua das vendas
-              [vendedor_numero_endereco] => 45
-              [vendedor_complemento] =>
-              [vendedor_bairro] => Centro
-              [vendedor_cidade] => Curitiba
-              [vendedor_estado] => PR
-              [vendedor_ativo] => 1
-              [vendedor_obs] =>
-              [vendedor_data_alteracao] => 2020-01-27 22:24:17
-             */
 
             $this->form_validation->set_rules('vendedor_nome_completo', 'Nome completo', 'trim|required|min_length[2]|max_length[200]');
             $this->form_validation->set_rules('vendedor_cpf', '', 'trim|required|exact_length[14]|callback_valida_cpf');
@@ -79,7 +53,6 @@ class Vendedores extends CI_Controller {
 
             if ($this->form_validation->run()) {
 
-//                exit('validado');
                 $data = elements(
                         array(
                             'vendedor_codigo',
@@ -122,6 +95,93 @@ class Vendedores extends CI_Controller {
                 $this->load->view('vendedores/edit');
                 $this->load->view('layout/footer');
             }
+        }
+    }
+
+    public function add() {
+        /*
+          [vendedor_id] => 1
+          [vendedor_codigo] => 09842571
+          [vendedor_data_cadastro] => 2020-01-27 22:24:17
+          [vendedor_nome_completo] => Lucio Antonio de Souza
+          [vendedor_cpf] => 946.873.070-00
+          [vendedor_rg] => 36.803.319-3
+          [vendedor_telefone] =>
+          [vendedor_celular] => (41) 99999-9999
+          [vendedor_email] => vendedor@gmail.com
+          [vendedor_cep] => 80530-000
+          [vendedor_endereco] => Rua das vendas
+          [vendedor_numero_endereco] => 45
+          [vendedor_complemento] =>
+          [vendedor_bairro] => Centro
+          [vendedor_cidade] => Curitiba
+          [vendedor_estado] => PR
+          [vendedor_ativo] => 1
+          [vendedor_obs] =>
+          [vendedor_data_alteracao] => 2020-01-27 22:24:17
+         */
+
+        $this->form_validation->set_rules('vendedor_nome_completo', 'Nome completo', 'trim|required|min_length[2]|max_length[200]');
+        $this->form_validation->set_rules('vendedor_cpf', '', 'trim|required|exact_length[14]|is_unique[vendedores.vendedor_cpf]|callback_valida_cpf');
+        $this->form_validation->set_rules('vendedor_rg', 'RG', 'trim|required|max_length[20]|is_unique[vendedores.vendedor_rg]');
+        $this->form_validation->set_rules('vendedor_email', 'E-mail', 'trim|required|valid_email|max_length[50]|is_unique[vendedores.vendedor_email]');
+        $this->form_validation->set_rules('vendedor_telefone', 'Telefone', 'trim|max_length[15]|is_unique[vendedores.vendedor_telefone]');
+        $this->form_validation->set_rules('vendedor_celular', 'Celular', 'trim|required|max_length[15]|is_unique[vendedores.vendedor_celular]');
+
+        $this->form_validation->set_rules('vendedor_endereco', 'Endereço', 'trim|required|max_length[155]');
+        $this->form_validation->set_rules('vendedor_numero_endereco', 'Número', 'trim|max_length[20]');
+        $this->form_validation->set_rules('vendedor_complemento', 'Complemento', 'trim|max_length[145]');
+        $this->form_validation->set_rules('vendedor_bairro', 'Bairro', 'trim|required|max_length[45]');
+        $this->form_validation->set_rules('vendedor_cidade', 'Cidade', 'trim|required|max_length[50]');
+        $this->form_validation->set_rules('vendedor_estado', 'UF', 'trim|required|exact_length[2]');
+        $this->form_validation->set_rules('vendedor_cep', 'Cep', 'trim|required|exact_length[9]');
+        $this->form_validation->set_rules('vendedor_obs', 'Observação', 'trim|max_length[800]');
+
+        if ($this->form_validation->run()) {
+
+//                exit('validado');
+            $data = elements(
+                    array(
+                        'vendedor_codigo',
+                        'vendedor_nome_completo',
+                        'vendedor_cpf',
+                        'vendedor_rg',
+                        'vendedor_email',
+                        'vendedor_telefone',
+                        'vendedor_celular',
+                        'vendedor_endereco',
+                        'vendedor_numero_endereco',
+                        'vendedor_complemento',
+                        'vendedor_bairro',
+                        'vendedor_cidade',
+                        'vendedor_estado',
+                        'vendedor_cep',
+                        'vendedor_ativo',
+                        'vendedor_obs',
+                    ), $this->input->post());
+
+
+            $data['vendedor_estado'] = strtoupper($this->input->post('vendedor_estado'));
+
+            $data = html_escape($data);
+
+            $this->core_model->insert('vendedores', $data);
+
+            redirect('vendedores');
+        } else {
+            $data = array(
+                'titulo' => 'Cadastrar vendedor',
+                'scripts' => array(
+                    'vendor/mask/jquery.mask.min.js',
+                    'vendor/mask/app.js',
+                ),
+                
+                'vendedor_codigo' => $this->core_model->generate_unique_code('vendedores', 'numeric', 8, 'vendedor_codigo'),
+            );
+
+            $this->load->view('layout/header', $data);
+            $this->load->view('vendedores/add');
+            $this->load->view('layout/footer');
         }
     }
 
