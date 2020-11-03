2d745fc276014419009831a517a2837d0f367273
jsapce <jamerson.developer@gmail.com>
2020-11-02 23:46:21 -0300

    Editando serviço

--- application/views/servicos/edit.php
+++ application/views/servicos/edit.php
@@ -11,7 +11,7 @@
 
         <nav aria-label="breadcrumb">
             <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="<?php echo base_url('vendedores') ?>">Vendedores cadastrados</a></li>
+                <li class="breadcrumb-item"><a href="<?php echo base_url('servicos') ?>">Serviços cadastrados</a></li>
                 <li class="breadcrumb-item active" aria-current="page"><?php echo $titulo ?></li>
             </ol>
         </nav>
@@ -20,132 +20,51 @@
         <div class="card shadow mb-4">
             <div class="card-body">
                 <form  class="user" method="POST" name="form_edit">
-                    <p><strong><i class="fas fa-clock"></i>&nbsp;&nbsp;Última alteração: </strong><?php echo formata_data_banco_com_hora($vendedor->vendedor_data_alteracao) ?></p>
+                    <p><strong><i class="fas fa-clock"></i>&nbsp;&nbsp;Última alteração: </strong><?php echo formata_data_banco_com_hora($servico->servico_data_alteracao) ?></p>
                     <fieldset class="mt-4 border p-2">
                         <legend class="font-small">
                             <i class="fas fa-user-friends"></i>&nbsp;Dados pessoais
                         </legend>
-                        <div class="form-group row">
-                            <div class="col-md-6">
-                                <label >Nome completo</label>
-                                <input type="text" class="form-control form-control-user" name="vendedor_nome_completo" value="<?php echo $vendedor->vendedor_nome_completo ?>">
-                                <?php echo form_error('vendedor_nome_completo', '<small class="form-text text-danger">', '</small>') ?>
-
-                            </div>
-                            <div class="col-md-3">
-                                <label >CPF</label>
-                                <input type="text" class="form-control form-control-user cpf" name="vendedor_cpf" value="<?php echo $vendedor->vendedor_cpf ?>">
-                                <?php echo form_error('vendedor_cpf', '<small class="form-text text-danger">', '</small>') ?>
-                            </div>
-                            <div class="col-md-3">
-                                <label >RG</label>
-                                <input type="text" class="form-control form-control-user rg" name="vendedor_rg" value="<?php echo $vendedor->vendedor_rg ?>">
-                                <?php echo form_error('vendedor_rg', '<small class="form-text text-danger">', '</small>') ?>
-                            </div>
-                        </div>
 
                         <div class="form-group row">
 
-                             <div class="col-md-6">
-                                <label >E-mail</label>
-                                <input type="email" class="form-control form-control-user" name="vendedor_email" value="<?php echo $vendedor->vendedor_email ?>">
-                                <?php echo form_error('vendedor_email', '<small class="form-text text-danger">', '</small>') ?>
+                            <div class="col-md-3">
+                                <label >Nome do serviço</label>
+                                <input type="text" class="form-control form-control-user" name="servico_nome" value="<?php echo $servico->servico_nome ?>">
+                                <?php echo form_error('servico_nome', '<small class="form-text text-danger">', '</small>') ?>
 
                             </div>
                             <div class="col-md-3">
-                                <label >Telefone fixo</label>
-                                <input type="text" class="form-control form-control-user sp_celphones" name="vendedor_telefone" value="<?php echo $vendedor->vendedor_telefone ?>">
-                                <?php echo form_error('vendedor_telefone', '<small class="form-text text-danger">', '</small>') ?>
+                                <label >Preço</label>
+                                <input type="text" class="form-control form-control-user money" name="servico_preco" value="<?php echo $servico->servico_preco ?>">
+                                <?php echo form_error('servico_preco', '<small class="form-text text-danger">', '</small>') ?>
                             </div>
                             <div class="col-md-3">
-                                <label >Celular</label>
-                                <input type="text" class="form-control form-control-user sp_celphones" name="vendedor_celular" value="<?php echo $vendedor->vendedor_celular ?>">
-                                <?php echo form_error('vendedor_celular', '<small class="form-text text-danger">', '</small>') ?>
-                            </div>
-
-                        </div>
-
-                    </fieldset>
-
-                    <fieldset class="mt-4 mb-3 border p-2">
-                        <legend class="font-small">
-                            <i class="fas fa-map-marker-alt"></i>&nbsp;Dados de endereço
-                        </legend>
-                        <div class="form-group row mb-3">
-                            <div class="col-md-6">
-                                <label >Logradouro</label>
-                                <input type="text" class="form-control form-control-user" name="vendedor_endereco" value="<?php echo $vendedor->vendedor_endereco ?>">
-                                <?php echo form_error('vendedor_endereco', '<small class="form-text text-danger">', '</small>') ?>
-                            </div>
-                            <div class="col-md-2">
-                                <label >Número</label>
-                                <input type="text" class="form-control form-control-user" name="vendedor_numero_endereco" value="<?php echo $vendedor->vendedor_numero_endereco ?>">
-                                <?php echo form_error('vendedor_numero_endereco', '<small class="form-text text-danger">', '</small>') ?>
-                            </div>
-                            <div class="col-md-4">
-                                <label >Complemento</label>
-                                <input type="text" class="form-control form-control-user" name="vendedor_complemento" value="<?php echo $vendedor->vendedor_complemento ?>">
-                                <?php echo form_error('vendedor_complemento', '<small class="form-text text-danger">', '</small>') ?>
-
-                            </div>
-
-                        </div>
-                        <div class="form-group row">
-                            <div class="col-md-4">
-                                <label >Bairro</label>
-                                <input type="text" class="form-control form-control-user" name="vendedor_bairro" value="<?php echo $vendedor->vendedor_bairro ?>">
-                                <?php echo form_error('vendedor_bairro', '<small class="form-text text-danger">', '</small>') ?>
-
-                            </div>
-                             <div class="col-md-2">
-                                <label >CEP</label>
-                                <input type="text" class="form-control form-control-user cep" name="vendedor_cep" value="<?php echo $vendedor->vendedor_cep ?>">
-                                <?php echo form_error('vendedor_cep', '<small class="form-text text-danger">', '</small>') ?>
-                            </div>
-                            <div class="col-md-5">
-                                <label >Cidade</label>
-                                <input type="text" class="form-control form-control-user" name="vendedor_cidade" value="<?php echo $vendedor->vendedor_cidade ?>">
-                                <?php echo form_error('vendedor_cidade', '<small class="form-text text-danger">', '</small>') ?>
-
-                            </div>
-                            <div class="col-md-1">
-                                <label >UF</label>
-                                <input type="text" class="form-control form-control-user uf" name="vendedor_estado" value="<?php echo $vendedor->vendedor_estado ?>">
-                                <?php echo form_error('vendedor_estado', '<small class="form-text text-danger">', '</small>') ?>
-
-                            </div>
-
-                        </div>
-                    </fieldset>
-
-                    <fieldset class="mt-4 mb-3 border p-2">
-                        <legend class="font-small">
-                            <i class="fas fa-tools"></i>&nbsp;Configurações
-                        </legend>
-                        <div class="form-group row mb-3">
-                            <div class="col-md-3">
-                                <label >Cliente ativo</label>
-                                <select class="custom-select" name="vendedor_ativo">
-                                    <option value="0" <?php echo $vendedor->vendedor_ativo == 0 ? 'selected' : '' ?>>Não</option>
-                                    <option value="1" <?php echo $vendedor->vendedor_ativo == 1 ? 'selected' : '' ?>>Sim</option>
+                                <label >Ativo</label>
+                                <select class="custom-select" name="servico_ativo">
+                                    <option value="0" <?php echo $servico->servico_ativo == 0 ? 'selected' : '' ?>>Não</option>
+                                    <option value="1" <?php echo $servico->servico_ativo == 1 ? 'selected' : '' ?>>Sim</option>
                                 </select>
                             </div>
-                             <div class="col-md-3">
-                                <label >Matrícula</label>
-                                <input type="text" class="form-control form-control-user" name="vendedor_codigo" value="<?php echo $vendedor->vendedor_codigo ?>" readonly="">
-                                <?php echo form_error('vendedor_codigo', '<small class="form-text text-danger">', '</small>') ?>
 
-                            </div>
-                            <div class="col-md-6">
-                                <label >Observação</label>
-                                <textarea class="form-control form-control-user" name="vendedor_obs"><?php echo $vendedor->vendedor_obs ?></textarea>
-                                <?php echo form_error('vendedor_obs', '<small class="form-text text-danger">', '</small>') ?>
-                            </div>
                         </div>
+
+                        <div class="form-group row">
+
+                            <div class="col-md-12">
+                                <label >Descrição do serviço</label>
+                                <textarea class="form-control form-control-user" name="servico_descricao" style="min-height: 100px!important;"><?php echo $servico->servico_descricao ?></textarea>
+                                <?php echo form_error('servico_descricao', '<small class="form-text text-danger">', '</small>') ?>
+                            </div>
+
+                        </div>
+
                     </fieldset>
+
                     <div class="form-group row mb-3">
-                        <input type="hidden" name="vendedor_id" value="<?php echo $vendedor->vendedor_id ?>">
+                        <input type="hidden" name="servico_id" value="<?php echo $servico->servico_id ?>">
                     </div>
+
                     <button type="submit" class="btn btn-primary btn-sm">Salvar</button>
                     <a title="Voltar" href="<?php echo base_url($this->router->fetch_class()) ?>" class="btn btn-sm btn-success ml-2">Voltar</a>
                 </form>
