a4c45a0baf44de397c332b380208918e5a2b2f9b
jsapce <jamerson.developer@gmail.com>
2020-12-03 19:17:34 -0300

    Criando documento para impressão - Parte 1

--- application/controllers/Ordem_servicos.php
+++ application/controllers/Ordem_servicos.php
@@ -33,34 +33,9 @@ class Ordem_servicos extends CI_Controller {
 
     public function edit($ordem_servico_id = NULL) {
         if (!$ordem_servico_id || !$this->core_model->get_by_id('ordens_servicos', array('ordem_servico_id' => $ordem_servico_id))) {
-            $this->session->set_flashdata('error', 'Oderm de serviço não encontrada');
+            $this->session->set_flashdata('error', 'Ordem de serviço não encontrada');
             redirect('os');
         } else {
-
-            /*
-              [ordem_servico_id] => 1
-              [ordem_servico_forma_pagamento_id] => 1
-              [ordem_servico_cliente_id] => 1
-              [ordem_servico_data_emissao] => 2020-02-14 17:30:35
-              [ordem_servico_data_conclusao] =>
-              [ordem_servico_equipamento] => Fone de ouvido
-              [ordem_servico_marca_equipamento] => Awell
-              [ordem_servico_modelo_equipamento] => AV1801
-              [ordem_servico_acessorios] => Mouse e carregador
-              [ordem_servico_defeito] => Não sai aúdio no lado esquerdo
-              [ordem_servico_valor_desconto] => R$ 0.00
-              [ordem_servico_valor_total] => 490.00
-              [ordem_servico_status] => 0
-              [ordem_servico_obs] =>
-              [ordem_servico_data_alteracao] => 2020-02-19 22:58:42
-              [cliente_id] => 1
-              [cliente_nome] => miria
-              [forma_pagamento_id] => 1
-              [forma_pagamento] => Cartão de crédito
-             */
-            //            echo '<pre>';
-//            print_r($ordem_servico);
-//            exit();
 
             $this->form_validation->set_rules('ordem_servico_cliente_id', '', 'required');
             $this->form_validation->set_rules('ordem_servico_forma_pagamento_id', '', 'required');
@@ -127,7 +102,7 @@ class Ordem_servicos extends CI_Controller {
                 }
 
 
-                redirect('os/imprimir/'. $ordem_servico_id);
+                redirect('os/imprimir/' . $ordem_servico_id);
             } else {
                 $data = array(
                     'titulo' => 'Atualizar ordem de serviço',
@@ -150,7 +125,13 @@ class Ordem_servicos extends CI_Controller {
                     'os_tem_servicos' => $this->ordem_servicos_model->get_all_servicos_by_ordem($ordem_servico_id),
                 );
 
+
+
                 $ordem_servico = $data['ordem_servico'] = $this->ordem_servicos_model->get_by_id($ordem_servico_id);
+
+                echo '<pre>';
+                print_r($data['ordem_servico']);
+                exit();
 
                 $this->load->view('layout/header', $data);
                 $this->load->view('ordem_servicos/edit');
@@ -164,15 +145,126 @@ class Ordem_servicos extends CI_Controller {
             $this->session->set_flashdata('error', 'Oderm de serviço não encontrada');
             redirect('os');
         } else {
-            
+
             $data = array(
                 'titulo' => 'Escolha uma opção',
             );
-            
-             $this->load->view('layout/header', $data);
-                $this->load->view('ordem_servicos/imprimir');
-                $this->load->view('layout/footer');
+
+            $this->load->view('layout/header', $data);
+            $this->load->view('ordem_servicos/imprimir');
+            $this->load->view('layout/footer');
         }
     }
 
+    public function pdf($ordem_servico_id = NULL) {
+        if (!$ordem_servico_id || !$this->core_model->get_by_id('ordens_servicos', array('ordem_servico_id' => $ordem_servico_id))) {
+            $this->session->set_flashdata('error', 'Oderm de serviço não encontrada');
+            redirect('os');
+        } else {
+            $empresa = $this->core_model->get_by_id('sistema', array('sistema_id' => 1));
+
+//            echo '<pre>';
+//            print_r($empresa);
+//            exit();
+
+            $ordem_servico = $this->ordem_servicos_model->get_by_id($ordem_servico_id);
+//            echo '<pre>';
+//            print_r($ordem_servico);
+//            exit();
+
+            $file_name = 'O.S&nmsp;' . $ordem_servico->ordem_servico_id;
+
+            $html = '<html>';
+
+            $html .= '<head>';
+            $html .= '<title>' . $empresa->sistema_nome_fantasia . ' | Impressão de ordem de serviço</title>';
+
+
+
+            $html .= '</head>';
+            
+            $html .= '<body style="font-size:12px">';
+            
+             $html .= '<h4>' . $empresa->sistema_razao_social . '</h4>';
+            
+            
+            $html .= '</body>';
+            
+             $html .= '<html>';
+
+            echo '<pre>';
+            print_r($html);
+            exit();
+        }
+    }
+
+    /*
+      stdClass Object
+      (
+      [ordem_servico_id] => 1
+      [ordem_servico_forma_pagamento_id] => 1
+      [ordem_servico_cliente_id] => 1
+      [ordem_servico_data_emissao] => 2020-02-14 17:30:35
+      [ordem_servico_data_conclusao] =>
+      [ordem_servico_equipamento] => Fone de ouvido
+      [ordem_servico_marca_equipamento] => Awell
+      [ordem_servico_modelo_equipamento] => AV1801
+      [ordem_servico_acessorios] => Mouse e carregador
+      [ordem_servico_defeito] => Não sai aúdio no lado esquerdo
+      [ordem_servico_valor_desconto] => R$ 8.00
+      [ordem_servico_valor_total] => 632.00
+      [ordem_servico_status] => 0
+      [ordem_servico_obs] =>
+      [ordem_servico_data_alteracao] => 2020-11-21 22:17:40
+      [cliente_id] => 1
+      [cliente_cpf_cnpj] => 366.293.560-04
+      [cliente_nome_completo] => miria fulano da silva
+      [forma_pagamento_id] => 1
+      [forma_pagamento] => Cartão de crédito
+      )
+     */
+
+    /*
+      stdClass Object
+      (
+      [sistema_id] => 1
+      [sistema_razao_social] => System bcit INC
+      [sistema_nome_fantasia] => Sistema natação now
+      [sistema_cnpj] => 56.565.646/4435-43
+      [sistema_ie] =>
+      [sistema_telefone_fixo] => (23) 42314-4123
+      [sistema_telefone_movel] => (98) 88888-8888
+      [sistema_email] => jamerson.developer@gmail.com
+      [sistema_site_url] =>
+      [sistema_cep] => 65556-565
+      [sistema_endereco] => Rua da natação,
+      [sistema_numero] => 123
+      [sistema_cidade] => São Luís
+      [sistema_estado] => MA
+      [sistema_txt_ordem_servico] => Serviços de qualidade
+      [sistema_data_alteracao] => 2020-12-03 18:58:48
+      )
+     */
+
+    /*
+      [ordem_servico_id] => 1
+      [ordem_servico_forma_pagamento_id] => 1
+      [ordem_servico_cliente_id] => 1
+      [ordem_servico_data_emissao] => 2020-02-14 17:30:35
+      [ordem_servico_data_conclusao] =>
+      [ordem_servico_equipamento] => Fone de ouvido
+      [ordem_servico_marca_equipamento] => Awell
+      [ordem_servico_modelo_equipamento] => AV1801
+      [ordem_servico_acessorios] => Mouse e carregador
+      [ordem_servico_defeito] => Não sai aúdio no lado esquerdo
+      [ordem_servico_valor_desconto] => R$ 0.00
+      [ordem_servico_valor_total] => 490.00
+      [ordem_servico_status] => 0
+      [ordem_servico_obs] =>
+      [ordem_servico_data_alteracao] => 2020-02-19 22:58:42
+      [cliente_id] => 1
+      [cliente_nome] => miria
+      [forma_pagamento_id] => 1
+      [forma_pagamento] => Cartão de crédito
+     */
 }
