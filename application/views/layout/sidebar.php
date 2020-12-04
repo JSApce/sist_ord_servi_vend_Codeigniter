a7b0915af5bdb6b68ec33301a4a8f9accc45b2f9
jsapce <jamerson.developer@gmail.com>
2020-12-04 17:19:44 -0300

    Listando vendas do banco de dados

--- application/views/layout/sidebar.php
+++ application/views/layout/sidebar.php
@@ -26,6 +26,7 @@
         <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
                 <h6 class="collapse-header">Escolha uma opção:</h6>
+                <a title="Gerenciar vendas"  class="collapse-item" href="<?php echo base_url('vendas'); ?>"><i class="fas fa-shopping-cart text-gray-900"></i>&nbsp;&nbsp;Vendas</a>
                 <a title="Gerenciar ordens de serviços"  class="collapse-item" href="<?php echo base_url('os'); ?>"><i class="fas fa-shopping-basket text-gray-900"></i>&nbsp;&nbsp;Ordem serviços</a>
             </div>
         </div>
