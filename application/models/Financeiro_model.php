1614be3bfd9d05beb628a85902b2340fe3df7895
jsapce <jamerson.developer@gmail.com>
2021-01-06 17:37:28 -0300

    Preparando model da home

--- application/models/Financeiro_model.php
+++ application/models/Financeiro_model.php
@@ -4,11 +4,22 @@ defined('BASEPATH') or exit('Ação não permitida!');
 
 class Financeiro_model extends CI_Model {
 
-    public function get_all_pagar() {
+//    public function get_all_pagar() {
+//        $this->db->select([
+//            'contas_pagar.*',
+//            'fornecedores.fornecedor_id',
+//            'fornecedores.fornecedor_nome_fantasia as fornecedor',
+//        ]);
+//        $this->db->join('fornecedores', 'fornecedor_id = conta_pagar_fornecedor_id', 'LEFT');
+//
+//        return $this->db->get('contas_pagar')->result();
+//    }
+    
+     public function get_all_pagar() {
         $this->db->select([
             'contas_pagar.*',
-            'fornecedores.fornecedor_id',
-            'fornecedores.fornecedor_nome_fantasia as fornecedor',
+            'fornecedor_id',
+            'fornecedor_nome_fantasia as fornecedor',
         ]);
         $this->db->join('fornecedores', 'fornecedor_id = conta_pagar_fornecedor_id', 'LEFT');
 
