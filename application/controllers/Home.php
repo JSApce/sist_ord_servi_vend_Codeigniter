d2578a8bab31d2d82640643c5ca6d9b98c3d3e1c
jsapce <jamerson.developer@gmail.com>
2021-01-06 21:15:47 -0300

    TrabalhaCriando central de notificaçoes - parte 1

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -11,23 +11,35 @@ class Home extends CI_Controller {
             $this->session->set_flashdata('info', 'Sua sessão expirou!');
             redirect('login');
         }
-        
+
         $this->load->model('home_model');
     }
 
     public function index() {
-        
+
         $data = array(
             'titulo' => 'Home',
-            
+            //HOME
             'soma_vendas' => $this->home_model->get_sum_vendas(),
             'soma_ordem_servicos' => $this->home_model->get_sum_ordem_servico(),
             'total_pagar' => $this->home_model->get_sum_pagar(),
             'total_receber' => $this->home_model->get_sum_receber(),
             'produtos_mais_vendidos' => $this->home_model->get_produtos_mais_vendidos(),
             'servicos_mais_vendidos' => $this->home_model->get_servicos_mais_vendidos(),
+                //CENTRAL DE NOTIFICAÇÕES
+//            'contas_receber_vencidas' => $this->home_model->get_contas_receber_vencidas(),
         );
-        
+
+        $contador_notificacoes = 0;
+
+        if ($this->home_model->get_contas_receber_vencidas()) {
+
+            $data['contas_receber_vencidas'] = TRUE;
+            $contador_notificacoes++;
+        }
+
+        $data['contador_notificacoes'] = $contador_notificacoes;
+
         $this->load->view('layout/header', $data);
         $this->load->view('home/index');
         $this->load->view('layout/footer');
--- application/views/home/index.php
+++ application/views/home/index.php
@@ -40,87 +40,90 @@
             </div>
         <?php endif; ?>
 
-        <!-- Content Row -->
-        <div class="row">
+        <?php if ($this->ion_auth->is_admin()): ?>
+            <!-- Content Row -->
+            <div class="row">
 
-            <!-- Earnings (Monthly) Card Example -->
-            <div class="col-xl-3 col-md-6 mb-4">
-                <div class="card border-left-success shadow h-100 py-2">
-                    <div class="card-body">
-                        <div class="row no-gutters align-items-center">
-                            <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Total de vendas</div>
-                                <div class="h5 mb-0 font-weight-bold text-success"><?php echo 'R$&nbsp;' . ($soma_vendas->venda_valor_total == NULL ? '0,00' : $soma_vendas->venda_valor_total); ?></div>
-                            </div>
-                            <div class="col-auto">
-                                <i class="fas fa-shopping-cart fa-3x text-success"></i>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <!-- Earnings (Monthly) Card Example -->
-            <div class="col-xl-3 col-md-6 mb-4">
-                <div class="card border-left-info shadow h-100 py-2">
-                    <div class="card-body">
-                        <div class="row no-gutters align-items-center">
-                            <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Total de serviços</div>
-                                <div class="h5 mb-0 font-weight-bold text-info"><?php echo 'R$&nbsp;' . ($soma_ordem_servicos->ordem_servico_valor_total == NULL ? '0,00' : $soma_ordem_servicos->ordem_servico_valor_total); ?></div>
-                            </div>
-                            <div class="col-auto">
-                                <i class="fas fa-shopping-basket fa-3x text-info"></i>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <!-- Earnings (Monthly) Card Example -->
-            <div class="col-xl-3 col-md-6 mb-4">
-                <div class="card border-left-danger shadow h-100 py-2">
-                    <div class="card-body">
-                        <div class="row no-gutters align-items-center">
-                            <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">Total a Pagar</div>
-                                <div class="row no-gutters align-items-center">
-                                    <div class="col-auto">
-                                        <div class="h5 mb-0 mr-3 font-weight-bold text-danger"><?php echo 'R$&nbsp;' . ($total_pagar->conta_pagar_valor == NULL ? '0,00' : $total_pagar->conta_pagar_valor); ?></div>
-                                    </div>
+                <!-- Earnings (Monthly) Card Example -->
+                <div class="col-xl-3 col-md-6 mb-4">
+                    <div class="card border-left-success shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                    <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Total de vendas</div>
+                                    <div class="h5 mb-0 font-weight-bold text-success"><?php echo 'R$&nbsp;' . ($soma_vendas->venda_valor_total == NULL ? '0,00' : $soma_vendas->venda_valor_total); ?></div>
+                                </div>
+                                <div class="col-auto">
+                                    <i class="fas fa-shopping-cart fa-3x text-success"></i>
                                 </div>
                             </div>
-                            <div class="col-auto">
-                                <i class="fas fa-money-bill-alt fa-3x text-danger"></i>
-                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <!-- Earnings (Monthly) Card Example -->
-            <div class="col-xl-3 col-md-6 mb-4">
-                <div class="card border-left-primary shadow h-100 py-2">
-                    <div class="card-body">
-                        <div class="row no-gutters align-items-center">
-                            <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Total a Receber</div>
-                                <div class="row no-gutters align-items-center">
-                                    <div class="col-auto">
-                                        <div class="h5 mb-0 mr-3 font-weight-bold text-primary"><?php echo 'R$&nbsp;' . ($total_receber->conta_receber_valor == NULL ? '0,00' : $total_receber->conta_receber_valor); ?></div>
-                                    </div>
+                <!-- Earnings (Monthly) Card Example -->
+                <div class="col-xl-3 col-md-6 mb-4">
+                    <div class="card border-left-info shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                    <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Total de serviços</div>
+                                    <div class="h5 mb-0 font-weight-bold text-info"><?php echo 'R$&nbsp;' . ($soma_ordem_servicos->ordem_servico_valor_total == NULL ? '0,00' : $soma_ordem_servicos->ordem_servico_valor_total); ?></div>
+                                </div>
+                                <div class="col-auto">
+                                    <i class="fas fa-shopping-basket fa-3x text-info"></i>
                                 </div>
                             </div>
-                            <div class="col-auto">
-                                <i class="fas fa-hand-holding-usd fa-3x text-primary"></i>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Earnings (Monthly) Card Example -->
+                <div class="col-xl-3 col-md-6 mb-4">
+                    <div class="card border-left-danger shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                    <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">Total a Pagar</div>
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col-auto">
+                                            <div class="h5 mb-0 mr-3 font-weight-bold text-danger"><?php echo 'R$&nbsp;' . ($total_pagar->conta_pagar_valor == NULL ? '0,00' : $total_pagar->conta_pagar_valor); ?></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-auto">
+                                    <i class="fas fa-money-bill-alt fa-3x text-danger"></i>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
+
+                <!-- Earnings (Monthly) Card Example -->
+                <div class="col-xl-3 col-md-6 mb-4">
+                    <div class="card border-left-primary shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Total a Receber</div>
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col-auto">
+                                            <div class="h5 mb-0 mr-3 font-weight-bold text-primary"><?php echo 'R$&nbsp;' . ($total_receber->conta_receber_valor == NULL ? '0,00' : $total_receber->conta_receber_valor); ?></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-auto">
+                                    <i class="fas fa-hand-holding-usd fa-3x text-primary"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
             </div>
 
-
-        </div>
+        <?php endif; ?>
 
         <!-- Content Row -->
         <div class="row">
@@ -152,9 +155,9 @@
 
                                         <tr>
                                             <td><?php echo $produto->produto_descricao ?></td>
-                                            <td class="text-center"><?php echo '<span class="badge badge-success">'. $produto->quantidade_vendidos . '</span>' ?></td>
+                                            <td class="text-center"><?php echo '<span class="badge badge-success">' . $produto->quantidade_vendidos . '</span>' ?></td>
                                         </tr>
-                                        
+
                                     <?php endforeach; ?>
                                 </tbody>
 
@@ -176,7 +179,7 @@
                         <div class="text-center">
                             <img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 9rem;" src="<?php echo base_url('public/img/servicos_vendidos.svg'); ?>" alt="">
                         </div>
-                        
+
                         <div class="table-responsive">
 
                             <table class="table table-striped table-borderless">
@@ -193,9 +196,9 @@
 
                                         <tr>
                                             <td><?php echo $servico->servico_nome ?></td>
-                                            <td class="text-center"><?php echo '<span class="badge badge-primary">'. $servico->quantidade_vendidos . '</span>' ?></td>
+                                            <td class="text-center"><?php echo '<span class="badge badge-primary">' . $servico->quantidade_vendidos . '</span>' ?></td>
                                         </tr>
-                                        
+
                                     <?php endforeach; ?>
                                 </tbody>
 
